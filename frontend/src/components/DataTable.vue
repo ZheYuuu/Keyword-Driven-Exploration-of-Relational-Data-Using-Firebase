484369a1dcb2bff10f6d8f707a2f3e551f179538
Yukai Jiang <yukaijia@usc.edu>
2020-04-26 14:57:49 -0700

    Update: fetch

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -13,7 +13,7 @@
   </a-table>
 </template>
 <script>
-  import reqwest from 'reqwest';
+  // import reqwest from 'reqwest';
   import axios from 'axios';
   const columns = [
     {
@@ -26,7 +26,7 @@
     {
       title: 'Area Code',
       dataIndex: 'area_code',
-      filters: [{ text: 'Male', value: 'male' }, { text: 'Female', value: 'female' }],
+      // filters: [{ text: 'Male', value: 'male' }, { text: 'Female', value: 'female' }],
       width: '20%',
     },
     {
@@ -57,57 +57,79 @@
       };
     },
     methods: {
+      getRecords(res) {
+        console.log('res', res);
+        let items = res.data.items;
+        let data = [];
+        for (const pk in items) {
+          let record = { uuid : pk };
+          for (const k in items[pk]) {
+            record[k] = items[pk][k];
+          }
+          data.push(record);
+        }
+        return data;
+      },
       getData() {
-        const path = 'http://localhost:5000/Daily';
+        const path = 'http://localhost:5000/api/coronavirus/daily_situation';
         axios.get(path)
           .then((res) => {
-            // const pagination = { ...this.pagination };
-            // // Read total count from server
-            // // pagination.total = data.totalCount;
-            // pagination.total = 200;
-            // this.loading = false;
-            this.data = res.data.data;
-            console.log('view data', this.data);
-            // this.pagination = pagination;
+            const pagination = { ...this.pagination };
+            const metadata = res.data._meta;
+            pagination.total = metadata.total_pages;
+            this.loading = false;
+            this.data = this.getRecords(res);
+            this.pagination = pagination;
           })
           .catch((error) => {
             console.error(error);
           });
       },
       handleTableChange(pagination, filters, sorter) {
-        console.log(pagination);
+        // console.log('handle table', pagination);
         const pager = { ...this.pagination };
         pager.current = pagination.current;
         this.pagination = pager;
+
         this.fetch({
-          results: pagination.pageSize,
-          page: pagination.current,
+          page_size: pagination.pageSize,
+          page: pagination.current - 1,
           sortField: sorter.field,
           sortOrder: sorter.order,
           ...filters,
         });
       },
       fetch(params = {}) {
-        console.log('params:', params);
         this.loading = true;
-        reqwest({
-          url: 'http://127.0.0.1:5000/Daily',
-          method: 'get',
-          data: {
-            results: 10,
-            ...params,
-          },
-          type: 'json',
-        }).then(data => {
-          const pagination = { ...this.pagination };
-          // Read total count from server
-          // pagination.total = data.totalCount;
-          pagination.total = 200;
-          this.loading = false;
-          this.data = data.results;
-          console.log('view data', this.data);
-          this.pagination = pagination;
-        });
+        const path = 'http://localhost:5000/api/coronavirus/daily_situation';
+        axios.get(path, { 
+          params: params,
+          })
+          .then((res) => {
+            this.data = this.getRecords(res);
+            this.loading = false;
+          })
+          .catch((error)=>{
+            console.error(error);
+          });
+        // reqwest({
+        //   url: 'http://localhost:5000/api/coronavirus/daily_situation',
+        //   method: 'get',
+        //   data: {
+        //     results: 10,
+        //     ...params,
+        //   },
+        //   type: 'json',
+        // }).then(data => {
+        //   const pagination = { ...this.pagination };
+        //   // Read total count from server
+        //   // pagination.total = data.totalCount;
+        //   pagination.total = 200;
+        //   this.loading = false;
+        //   this.data = data.results;
+        //   console.log('view data', this.data);
+        //   this.pagination = pagination;
+        // });
       },
     },
   };
