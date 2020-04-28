2f1467e93b65a99539db7bbafd60da9d8a0a54eb
Yukai Jiang <yukaijia@usc.edu>
2020-04-27 23:38:25 -0700

    Feature: Search and Reference jump.

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -1,29 +1,53 @@
 <template>
   <div>
-    <div>Database:{{data_base}} Table:{{data_table}}</div>
-    <a-table
-      :columns="columns"
-      :rowKey="record => record.uuid"
-      :dataSource="data"
-      :pagination="pagination"
-      :loading="loading"
-      @change="handleTableChange"
-    >
-      <!-- <template slot="name" slot-scope="name">
-        {{name.first}} {{name.last}}
-      </template> -->
+    <div>Database:{{data_base}} Table:{{data_table_name}}</div>
+      <a-table
+        :columns="columns"
+        :rowKey="record => record.uuid"
+        :dataSource="data"
+        :pagination="pagination"
+        :loading="loading"
+        :scroll="{ x: 1000}"
+        @change="handleTableChange"
+      >
+      <a slot="fk" slot-scope="text, record, tags, node" v-on:click="handleClick(text, record, tags, node)">{{ text }}</a>
     </a-table>
   </div>
 </template>
 <script>
   // import reqwest from 'reqwest';
   import axios from 'axios';
+
+  const pk_map = {
+    "city": ["ID"],
+    "country": ["Code"],
+    "countrylanguage": ["CountryCode", "Language"],
+    "daily_situation": ["area_code", "date"],
+    "statisic": ["date"],
+    "area": ["area_code"],
+    "train_info": ["ID"],
+    "prov_railway_info": ["province"],
+    "station_info": ["telecode"],
+  };
+
+  const fk_map = {
+    "city": {"CountryCode": "country-Code"},
+    "country": {},
+    "countrylanguage": {"CountryCode": "country-Code"},
+    "daily_situation": {"area_code": "area-area_code", "date": "statistic-date"},
+    "statisic": {},
+    "area": {},
+    "train_info": {"start":"station_info-telecode", "end":"station_info-telecode"},
+    "prov_railway_info": {},
+    "station_info": {"province_pinyin": "prov_railway_info-province"},
+  }
   
   export default {
     props:["data_base", "data_table", "keyword", "entrys"],
     watch: { 
       data_table: function(newVal, oldVal) { 
         console.log("change datatable2!", newVal, oldVal);
+        this.data_table_name = this.data_table;
         this.getData();
       },
       data_base: function(newVal, oldVal) { 
@@ -38,36 +62,104 @@
     },
     mounted() {
       if (this.entrys.length > 0) {
-        console.log("show entry!");
+        // console.log("show entry!");
         this.showSearch();
       }
       else {
-        console.log("get data!");
+        // console.log("get data!");
+        this.data_table_name = this.data_table;
         this.getData();
       }
       
     },
     data() {
       let columns = [];
+      let data_table_name;
       return {
         data: [],
         pagination: {},
         loading: false,
         columns,
+        data_table_name
       };
     },
     methods: {
-      getColumns(data) {
+      handleClick(text, record, tags, node) {
+        // console.log("Click!", text, record, tags, node);
+        let id = text;
+        let fk = node.dataIndex;
+        let out_obj = fk_map[this.data_table_name][fk].split('-');
+        let new_table = out_obj[0];
+        let path = this.getPath(id.toLowerCase(), new_table);
+        this.data_table_name = new_table;
+        axios.get(path)
+        .then((res) => {
+          console.log('click', res);
+          res.data['uuid'] = id.toString();
+          let data = res.data;
+          this.pagination = {
+            total: 1,
+            current: 0,
+            pageSize: 10
+          };
+          console.log('click', data);
+          this.data = [data];
+          this.columns = this.getColumns(this.data, new_table);
+          
+          this.loading = false;
+          console.log("click Done.")
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+      },
+
+      getColumns(data, table=this.data_table_name) {
         let record = data[0];
         let columns = [];
-        for (let k in record) {
-          if (k!='uuid') {
+        let key_set = ['uuid'];
+        console.log(pk_map, fk_map);
+        
+        // let base = this.data_base;
+        // add pk
+        for (let pk of pk_map[table]) {
+          columns.push({
+            title: pk,
+            dataIndex: pk,
+            // scopedSlots: { customRender: ['pk'] },
+          });
+          key_set.push(pk);
+        }
+        // add fk
+        for (let fk in fk_map[table]) {
+          // if it is also a pk
+          if (key_set.indexOf(fk) != -1) {
+            for (let i = 0; i < columns.length; i++) {
+              if(columns[i]['title'] == fk) {
+                columns[i]['scopeSlots'] = { customRender: 'fk' };
+              }
+            }
+          }
+          else {
+            console.log(fk);
             columns.push({
-              title: k,
-              dataIndex: k
-            })
+              title: fk,
+              dataIndex: fk,
+              scopedSlots: { customRender: 'fk' },
+            });
+            key_set.push(fk);
           }
         }
+
+        for (let k in record) {
+          if (key_set.indexOf(k) == -1) {
+              columns.push({
+                title: k,
+                dataIndex: k,
+              });
+          }
+        }
+        console.log("columns", columns);
         return columns;
       },
       getRecords(res) {
@@ -84,12 +176,15 @@
         return data;
       },
 
-      getPath(pk){
+      getPath(pk=null, table=null){
+        if (table != null) {
+          return 'http://54.219.171.61:5000/api/' + this.data_base + '/' + table + '/' + pk;
+        }
         if (pk != null) {
-          return 'http://54.219.171.61:5000/api/' + this.data_base + '/' + this.data_table + '/' + pk;
+          return 'http://54.219.171.61:5000/api/' + this.data_base + '/' + this.data_table_name + '/' + pk;
         }
         else {
-          return 'http://54.219.171.61:5000/api/' + this.data_base + '/' + this.data_table;
+          return 'http://54.219.171.61:5000/api/' + this.data_base + '/' + this.data_table_name;
         }
       },
 
@@ -99,7 +194,7 @@
         // console.log(path);
         axios.get(path)
           .then((res) => {
-            console.log(res);
+            // console.log(res);
             const metadata = res.data._meta;
             const pagination = {
               total: metadata.total_pages,
@@ -110,6 +205,7 @@
             this.data = this.getRecords(res);
             this.columns = this.getColumns(this.data);
             this.pagination = pagination;
+            console.log("getData done.");
           })
           .catch((error) => {
             console.error(error);
@@ -120,6 +216,7 @@
         const pager = { ...this.pagination };
         pager.current = pagination.current;
         this.pagination = pager;
+        console.warn("Im here!");
 
         this.fetch({
           page_size: pagination.pageSize,
@@ -128,6 +225,7 @@
           sortOrder: sorter.order,
           ...filters,
         });
+        console.log("handleChange done.");
       },
       fetch(params = {}) {
         this.loading = true;
@@ -139,6 +237,7 @@
             this.data = this.getRecords(res);
             this.columns = this.getColumns(this.data);
             this.loading = false;
+            console.log("fetch done.");
           })
           .catch((error)=>{
             console.error(error);
@@ -147,9 +246,10 @@
       showSearch() {
         this.loading = true;
         let data = [];
-        console.log("show entrys", this.entrys);
+        // console.warn("show entrys", this.entrys);
         for (let entry of this.entrys) {
           let path = this.getPath(entry);
+          // console.warn("shw path", path);
           axios.get(path)
           .then((res) => {
             // console.log('entry', res);
@@ -171,23 +271,15 @@
             console.error(error);
           })
         }
-        // const path = this.getPath(search_word);
-        // this.loading = true;
-        // // console.log(path);
-        // axios.get(path)
-        //   .then((res) => {
-        //     console.log(res);
-        //     const metadata = res.data._meta;
-        //     const pagination = {
-        //       total: metadata.total_pages,
-        //       current: 0,
-        //       pageSize: 10
-        //     };
-        //   })
-        //   .catch((error) => {
-        //     console.error(error)
-        //   });
-      }
+      },
+      
     },
   };
 </script>
+
+<style scoped>
+  /* .is-pk {
+    font-weight:bold; 
+    text-decoration-line: underline;
+  } */
+</style>
